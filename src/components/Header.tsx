8e53a8be3eca7c309871bd64603cd570f16d8a8f
OLors <panuwhich.a@gmail.com>
2025-01-02 22:27:57 +0700

    add pages router

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Button } from "../components/ui/button";
+import cheflogo from "../img/cheflogo.png";
+
+export default function Header() {
+  return (
+    <div className="container bg-[#c9945b] w-full h-36 rounded-t-3xl flex justify-center items-center relative">
+      <div className="text-6xl cursor-pointer pl-10 font-patrick text-[#fcf4d3]">
+        How2Cooking
+      </div>
+      <div>
+        <img src={cheflogo} className="w-72 " />
+      </div>
+
+      <div className="text-lg pr-4 font-patrick justify-center items-center text-[#fcf4d3] flex">
+        <div className="mx-4 cursor-pointer">CATEGORIES</div>
+        <div className="mx-4 cursor-pointer">RANDOM MEAL</div>
+
+        <div className="mx-4">
+          <input
+            type="text"
+            className="bg-transparent border-4 border-[fcf4d3] outline-none w-full p-2 border-solid rounded-lg"
+          />
+        </div>
+        <div className="">
+          <Button className="bg-[#9d784a] border-[#b39561] cursor-pointer hover:bg-yellow-700 border-solid border-2">
+            SEARCH
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import ShowMeal from "../components/ShowMeal";
+
+const HomePage = () => {
+  return (
+    <div>
+        <div className="bg-[#f6e5bf] p-20 flex justify-center mx-auto  container w-full rounded-b-3xl">
+          <ShowMeal />
+        </div>
+      
+    </div>
+  );
+};
+
+export default HomePage;
