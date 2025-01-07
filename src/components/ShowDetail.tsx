a7b30ababe94a6142efb69721a3ae8a11032e0d9
devweight100 <panuwhich.a@gmail.com>
2025-01-07 18:00:13 +0700

    แก้ไข showDetail

--- src/components/ShowDetail.tsx
+++ src/components/ShowDetail.tsx
@@ -26,112 +26,146 @@ export default function ShowDetail({ props }: ShowDetailProps) {
       }
     })
     .filter(Boolean);
-  
-   const [isLiked, setIsLiked] = useState(false);
-   const { likeIds, setLikeIds } = useContext<MealContextType>(MealContext);
 
-   const handleLike = () => {
-     setIsLiked(!isLiked);
-     if (!isLiked) {
-       setLikeIds([...likeIds, props]);
-     } else {
-       setLikeIds(likeIds.filter((ele) => ele.idMeal !== props.idMeal));
-     }
-   };
-   useEffect(() => {
-     // console.log(likeIds);
-     if (likeIds.some((like) => like.idMeal === props.idMeal)) {
-       setIsLiked(true);
-     }
-   }, [likeIds.length]);
+  const [isLiked, setIsLiked] = useState(false);
+  const { likeIds, setLikeIds } = useContext<MealContextType>(MealContext);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+    if (!isLiked) {
+      setLikeIds([...likeIds, props]);
+    } else {
+      setLikeIds(likeIds.filter((ele) => ele.idMeal !== props.idMeal));
+    }
+  };
+  useEffect(() => {
+    // console.log(likeIds);
+    if (likeIds.some((like) => like.idMeal === props.idMeal)) {
+      setIsLiked(true);
+    }
+  }, [likeIds.length]);
   return (
     <div>
-      <div className="flex justify-center w-[30%] mx-auto pb-5">
-        <img
-          className="rounded-3xl"
-          src={props.strMealThumb}
-          alt={props.strMeal}
-        />
-      </div>
+      <div className="flex">
+        <div className="w-[70%] pr-16">
+          <div className="flex justify-center w-[40%] mx-auto pb-5">
+            <img
+              className="rounded-3xl"
+              src={props.strMealThumb}
+              alt={props.strMeal}
+            />
+          </div>
 
-      <div className="flex justify-center my-5">
-        <h1 className="text-3xl cursor-pointer pl-10 font-patrick text-[#645414]">
-          {props.strMeal}
-        </h1>
-        <div
-          className="flex items-center mx-2"
-          onClick={() => handleLike()}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill={isLiked ? "red" : "none"}
-            stroke="red"
-            strokeWidth="1"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="w-8 h-8"
-          >
-            <path d="M20.8 4.6a5.4 5.4 0 00-7.6 0L12 5.8l-1.2-1.2a5.4 5.4 0 00-7.6 7.6L12 21l8.8-8.8a5.4 5.4 0 000-7.6z" />
-          </svg>
-        </div>
-      </div>
+          <div className="flex justify-center my-5">
+            <h1 className="text-2xl pl-10 font-patrick cursor-default text-[#645414]">
+              {props.strMeal}
+            </h1>
+            <div
+              className="flex items-center mx-2 cursor-pointer"
+              onClick={() => handleLike()}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill={isLiked ? "red" : "none"}
+                stroke="red"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-8 h-8"
+              >
+                <path d="M20.8 4.6a5.4 5.4 0 00-7.6 0L12 5.8l-1.2-1.2a5.4 5.4 0 00-7.6 7.6L12 21l8.8-8.8a5.4 5.4 0 000-7.6z" />
+              </svg>
+            </div>
+          </div>
 
-      <p className="indent-20 font-patrick text-[#493f18]">
-        {" "}
-        {props.strInstructions}
-      </p>
-      {props.strYoutube && (
-        <div className="flex items-center flex-col py-10 font-patrick  text-[#645414] text-xl">
-          <h2 className="pb-5">Recipe Video</h2>
-          <iframe
-            width="672"
-            height="378"
-            src={`https://www.youtube.com/embed/${
-              props.strYoutube.split("v=")[1]
-            }`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Recipe Video"
-          ></iframe>
+          <p className="indent-20 font-patrick text-sm text-[#493f18]">
+            {" "}
+            {props.strInstructions}
+          </p>
+          {props.strYoutube && (
+            <div className="flex items-center flex-col pt-5 font-patrick  text-[#645414] text-xl">
+              <iframe
+                width="672"
+                height="378"
+                src={`https://www.youtube.com/embed/${
+                  props.strYoutube.split("v=")[1]
+                }`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Recipe Video"
+              ></iframe>
+            </div>
+          )}
         </div>
-      )}
-      {ingredients.length > 0 ? (
-        <div className="mt-2 text-center px-60">
-          <h1 className="text-2xl underline text-bold font-patrick text-amber-900">
-            Ingredients Table
-          </h1>
-          <Table className="p-5 my-5 text-center border-2 border-collapse border-solid border-amber-700">
-            <TableHeader className="text-xl border-2 border-collapse border-solid font-patrick text-bold text-amber-700 border-amber-700">
-              <TableRow>
-                <TableCell className="border-2 border-solid border-amber-700">
-                  Ingredients
-                </TableCell>
-                <TableCell>Measure</TableCell>
-              </TableRow>
-            </TableHeader>
-            <TableBody className="text-base font-patrick text-[#493f18] ">
-              {ingredients.map((ingre, i) => (
-                <TableRow
-                  key={ingre?.ingredient?.concat(`${i}`)}
-                  className="border-0"
-                >
-                  <TableCell className="border-solid border-x border-amber-700">
-                    <div className="flex justify-center">
-                      <img
-                        src={`https://www.themealdb.com/images/ingredients/${ingre?.ingredient}-Small.png`}
-                      />
-                    </div>
-                    <div>{ingre?.ingredient}</div>
-                  </TableCell>
-                  <TableCell>{ingre?.measure}</TableCell>
+        <div className="w-[30%]">
+          {ingredients.length > 0 ? (
+            <div className="">
+              <Table className="p-5 mb-5 border-2 mx-auto w-[100%] border-solid border-amber-700">
+                <TableHeader className="text-base border-2 border-collapse border-solid font-patrick text-bold text-amber-700 border-amber-700">
+                  <TableRow>
+                    <TableCell className="text-center">Ingredients</TableCell>
+                    <TableCell className="text-center">Measure</TableCell>
+                  </TableRow>
+                </TableHeader>
+                <TableBody className="text-base font-patrick text-[#493f18] ">
+                  {ingredients.map((ingre, i) => (
+                    <TableRow
+                      key={ingre?.ingredient?.concat(`${i}`)}
+                      className="h-0 border-b-0"
+                    >
+                      <TableCell className="flex p-2 text-center">
+                        <div className="flex justify-center mx-6">
+                          <img
+                            className="object-cover w-12"
+                            src={`https://www.themealdb.com/images/ingredients/${ingre?.ingredient}-Small.png`}
+                          />
+                        </div>
+                        <div className="flex items-center justify-start text-sm">
+                          {ingre?.ingredient}
+                        </div>
+                      </TableCell>
+                      <TableCell className="text-sm text-center">
+                        {ingre?.measure}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              {/* <div className="container w-screen overflow-x-hidden">
+            <Table className="w-full max-w-full p-5 mx-auto my-5 overflow-hidden border-2 border-solid border-amber-700">
+              <TableHeader className="text-lg font-bold border-2 border-collapse border-solid font-patrick text-amber-700 border-amber-700"></TableHeader>
+              <TableBody className="text-base font-patrick text-[#493f18]">
+                <TableRow>
+                  <TableCell className="text-center">Ingredients</TableCell>
+                  {ingredients.map((ingre, i) => (
+                    <TableCell
+                      className="p-2 text-xs text-center"
+                      key={ingre?.ingredient?.concat(`${i}`)}
+                    >
+                      <div className="flex justify-center mx-6">
+                        <img
+                          className="w-8"
+                          src={`https://www.themealdb.com/images/ingredients/${ingre?.ingredient}-Small.png`}
+                        />
+                      </div>
+                      <div>{ingre?.ingredient}</div>
+                    </TableCell>
+                  ))}
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+                <TableRow>
+                  <TableCell className="text-center">Measure</TableCell>
+                  <TableCell>Heel</TableCell>
+                  <TableCell>Heel</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div> */}
+            </div>
+          ) : null}
         </div>
-      ) : null}
+      </div>
     </div>
   );
 }
